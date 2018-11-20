05348d348fa669be099a1a6df9eeece347b5203f
hiroshimuto <hiroshi.github@gmail.com>
2018-11-20 16:34:14 +0900

    Add pagenation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,18 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC").page(params[:page]).per(3)
+    #変数postsを定義する。Postモデルからすべてのデータを取得し、orderメソッドで最新の投稿順にする。pageメソッドはビューのリクエストの際にparamsの中にpageというキーが追加されて、その値がビューで指定したページ番号となる。perメソッドは1ページに表示したい件数を指定。
   end
 
   def new
     @post = Post.new
+    #新規投稿のnewメソッドを記載。
   end
 
   def create
     Post.create(post_params)
+    #new.html.erbでsubmitが押されるとcreateアクションが実行される。パラメーターが送られてくる。privateメソッドで受け取るパラメーターを制限。
   end
 
   private
