cea64fdb6c5a3fd6e303eb0d90f76ffb971ca806
hiroshimuto <hiroshi.github@gmail.com>
2018-11-21 16:52:14 +0900

    Add comment form and create action

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to "/posts/#{@comment.post.id}"
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(post_id: params[:post_id], user_id: current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :posts do
-    resources :comments, only: [:create]
+    resources :comments, only: [:new, :create]
   end
   resources :users, only: [:show]
 end
