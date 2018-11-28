5ab5afb063ed9016dbe36f3a200a78bef037c802
hiroshimuto <hiroshi.github@gmail.com>
2018-11-28 14:04:24 +0900

    Correct db columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123032056) do
+ActiveRecord::Schema.define(version: 20181128050207) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",                  null: false
@@ -30,12 +30,12 @@ ActiveRecord::Schema.define(version: 20181123032056) do
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "title",      limit: 65535
     t.text     "content",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "image",      limit: 65535
     t.integer  "user_id"
+    t.text     "title",      limit: 65535
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
